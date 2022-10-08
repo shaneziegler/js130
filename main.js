e6734e9d2115310e24e602286dc6eb366b1e326a
Shane-Ziegler <shanedb@gmail.com>
2022-10-08 12:29:31 -0500

    sat

---
+++ main.js
@@ -0,0 +1,6 @@
+const { push, pop, arr } = require("./foo");
+
+push(3);
+push(2);
+arr[2] = 5;
+console.log(pop());
---
+++ modules.js
@@ -0,0 +1,5 @@
+console.log(module);
+console.log(module.path);
+console.log(exports);
+console.log(__dirname);
+console.log(__filename);
